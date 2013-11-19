fe955459865cfc23ec49c81db1931276b615535c
Silas Baronda <silas.baronda@gmail.com>
2013-11-19 17:56:30 -0500

    Adding migration to remove need of index on username

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131117161503) do
+ActiveRecord::Schema.define(version: 20131119225356) do
 
   create_table "tenants", force: true do |t|
     t.string   "subdomain"
@@ -36,7 +36,6 @@ ActiveRecord::Schema.define(version: 20131117161503) do
     t.integer  "tenant_id"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   add_index "users", ["tenant_id"], name: "index_users_on_tenant_id"
 
